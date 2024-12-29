50e7d7a4244e8e1fa757ef09dd8508371a66395f
mercy-rotich <cherotichm182@gmail.com>
2024-12-28 23:54:35 -0800

    changes

---
+++ src/components/Weather.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='Search'/>
+            <img src={search_icon} alt="" />
+        </div>
+
+        <img src="" alt="" />
+        
+    </div>
+  )
+}
+
+export default Weather
